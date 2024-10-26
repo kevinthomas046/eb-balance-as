81235b6f064a81fb7992bd9b3af6a4b8bca3925c
Kevin Thomas <kevin.thomas@razorfish.com>
2024-10-26 16:38:22 -0400

    feat: getBalancesByDate API and FE integration

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,16 @@
 const SPREADSHEET_ID: string =
   PropertiesService.getScriptProperties().getProperty('SHEET_ID') || '';
 
+const SHEETS = {
+  FAMILIES: 'Families',
+  STUDENTS: 'Students',
+  ATTENDANCE: 'Attendance',
+  PAYMENTS: 'Payments',
+  CLASSES: 'Classes',
+  CLASS_GROUPS: 'ClassGroups',
+  ADDITIONAL_FEES: 'AdditionalFees',
+};
+
 /**
  * Special function that handles HTTP GET requests to the published web app.
  * @return {GoogleAppsScript.HTML.HtmlOutput} The HTML page to be served.
@@ -26,9 +36,10 @@ const SPREADSHEET_ID: string =
 /* eslint-disable @typescript-eslint/no-unused-vars */
 function doGet() {
   Logger.log('Loading page');
+  console.log(SpreadsheetApp.getActiveSpreadsheet().getName());
   return HtmlService.createTemplateFromFile('page')
     .evaluate()
-    .setTitle('Student Portal | Elevation Beats Inc');
+    .setTitle('Balance Admin | Elevation Beats Inc');
 }
 
 /**
@@ -54,13 +65,125 @@ function include(filename: string) {
 /**
  * Retrieves a reference to a Google sheet by name.
  * @param {string} sheetName Name of the sheet to retrieve
- * @returns {GoogleAppsScript.Spreadsheet.Sheet} Google sheet reference
  */
-function getSheetByName(sheetName: string): GoogleAppsScript.Spreadsheet.Sheet {
-  const sheet =
-    SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName(sheetName);
+function getSheetByName(
+  sheetName: string
+): ReturnType<GoogleAppsScript.Spreadsheet.Range['getValues']> {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
   if (!sheet) {
     throw new Error(`Sheet with name ${sheetName} not found.`);
   }
-  return sheet;
+  return sheet.getDataRange().getValues().slice(1);
+}
+
+function getBalancesByDate(filterDate: string) {
+  const parsedFilterDate = Date.parse(filterDate);
+
+  const familyData = getSheetByName(SHEETS.FAMILIES);
+  const attendanceData = getSheetByName(SHEETS.ATTENDANCE);
+  const studentsData = getSheetByName(SHEETS.STUDENTS);
+  const classData = getSheetByName(SHEETS.CLASSES);
+  const classGroupData = getSheetByName(SHEETS.CLASS_GROUPS);
+  const additionalFeesData = getSheetByName(SHEETS.ADDITIONAL_FEES);
+  const paymentData = getSheetByName(SHEETS.PAYMENTS);
+
+  const groupedStudents = studentsData.reduce(
+    (groupedStudents, [id, , familyId]) => {
+      groupedStudents[familyId] ??= [];
+      groupedStudents[familyId].push(id);
+      return groupedStudents;
+    },
+    {} as Record<string, string[]>
+  );
+
+  const groupedClassGroups = classGroupData.reduce(
+    (groupedClassGroups, [id, name, price]) => {
+      groupedClassGroups[id] = {
+        id,
+        name,
+        price,
+      };
+      return groupedClassGroups;
+    },
+    {} as Record<string, Record<'id' | 'name' | 'price', string | number>>
+  );
+
+  const expandedAttendanceData = attendanceData.reduce(
+    (expandedAttendanceData, [id, studentId, classId, , price]) => {
+      const [, classGroupId, classDate, classPrice] =
+        classData.find(([id]) => id === classId) ?? [];
+      const calculatedPrice =
+        price !== ''
+          ? price
+          : classPrice || groupedClassGroups[classGroupId].price;
+      expandedAttendanceData[id] = {
+        studentId,
+        date: classDate,
+        price: calculatedPrice,
+      };
+      return expandedAttendanceData;
+    },
+    {} as Record<
+      string,
+      Record<'studentId' | 'date' | 'price', string | number>
+    >
+  );
+
+  return familyData.reduce(
+    (familyBalance, [familyId, familyName]) => {
+      if (familyName === '') return familyBalance;
+
+      const studentsInFamily = groupedStudents[familyId];
+      const classBalance = Object.values(expandedAttendanceData).reduce(
+        (classBalance, { studentId, date, price }) => {
+          if (
+            studentsInFamily.includes(studentId as string) &&
+            parsedFilterDate >= Date.parse(date as string)
+          ) {
+            classBalance += Number(price);
+          }
+          return classBalance;
+        },
+        0
+      );
+      const additionalFees = additionalFeesData.reduce(
+        (additionalFees, [, studentId, feeDate, , price]) => {
+          if (
+            studentsInFamily.includes(studentId) &&
+            parsedFilterDate >= Date.parse(feeDate)
+          ) {
+            additionalFees += Number(price);
+          }
+          return additionalFees;
+        },
+        0
+      );
+      const paymentTotal = paymentData.reduce(
+        (paymentTotal, [, attendanceFamilyId, paymentDate, amountPaid]) => {
+          if (
+            attendanceFamilyId === familyId &&
+            parsedFilterDate >= Date.parse(paymentDate)
+          ) {
+            paymentTotal += Number(amountPaid);
+          }
+          return paymentTotal;
+        },
+        0
+      );
+      const balance = classBalance + additionalFees - paymentTotal;
+
+      familyBalance.push({
+        name: familyName,
+        classBalance,
+        additionalFees,
+        paymentTotal,
+        balance,
+      });
+      return familyBalance;
+    },
+    [] as Record<
+      'name' | 'classBalance' | 'additionalFees' | 'paymentTotal' | 'balance',
+      number
+    >[]
+  );
 }
