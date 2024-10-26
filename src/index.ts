58e87841802ccbc106f23343c7ec3d353e1e109e
Kevin Thomas <kevin.thomas@razorfish.com>
2024-10-26 13:52:18 -0400

    chore: initial artifacts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+const SPREADSHEET_ID: string =
+  PropertiesService.getScriptProperties().getProperty('SHEET_ID') || '';
 
 /**
  * Special function that handles HTTP GET requests to the published web app.
@@ -21,28 +25,42 @@
  */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 function doGet() {
-    Logger.log('Loading page');
-    return HtmlService.createTemplateFromFile('page')
-      .evaluate()
-      .setTitle('Balance | Elevation Beats Inc');
+  Logger.log('Loading page');
+  return HtmlService.createTemplateFromFile('page')
+    .evaluate()
+    .setTitle('Student Portal | Elevation Beats Inc');
+}
+
+/**
+ * Includes template based on filename that has a nested include
+ * @param filename file name to be included
+ * @returns {GoogleAppsScript.HTML.HtmlOutput}
+ */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+function includeTemplate(filename: string) {
+  return HtmlService.createTemplateFromFile(filename).evaluate().getContent();
+}
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/**
+ * Includes template based on filename
+ * @param filename file name to be included
+ * @returns {GoogleAppsScript.HTML.HtmlOutput}
+ */
+function include(filename: string) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+/**
+ * Retrieves a reference to a Google sheet by name.
+ * @param {string} sheetName Name of the sheet to retrieve
+ * @returns {GoogleAppsScript.Spreadsheet.Sheet} Google sheet reference
+ */
+function getSheetByName(sheetName: string): GoogleAppsScript.Spreadsheet.Sheet {
+  const sheet =
+    SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName(sheetName);
+  if (!sheet) {
+    throw new Error(`Sheet with name ${sheetName} not found.`);
   }
-  
-  /**
-   * Includes template based on filename that has a nested include
-   * @param filename file name to be included
-   * @returns {GoogleAppsScript.HTML.HtmlOutput}
-   */
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-  function includeTemplate(filename: string) {
-    return HtmlService.createTemplateFromFile(filename).evaluate().getContent();
-  }
-  
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-  /**
-   * Includes template based on filename
-   * @param filename file name to be included
-   * @returns {GoogleAppsScript.HTML.HtmlOutput}
-   */
-  function include(filename: string) {
-    return HtmlService.createHtmlOutputFromFile(filename).getContent();
-  }
+  return sheet;
+}
